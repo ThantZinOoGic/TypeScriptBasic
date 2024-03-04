f9193b07a32e5bd463b03e8ce302e8e9730cfec3
ThantZinOoGic <thantzinoo@gicjp.com>
2024-03-04 14:12:27 +0630

    edit config

---
+++ dist/index.js
@@ -0,0 +1,14 @@
+"use strict";
+let person = "Mg Mg";
+let age = 23;
+// let age = "Hello";
+let obj = {
+    name: "Ag Ag",
+    age: 22,
+    single: true
+};
+// obj.age = "Hello";
+let arr = [1, 2, 3, 4];
+// arr.push("Hello");
+console.log(arr);
+console.log(obj);
---
+++ src/index.ts
@@ -0,0 +1,18 @@
+let person  : String = "Mg Mg";
+let age : Number = 23;
+// let age = "Hello";
+
+let obj = {
+    name : "Ag Ag",
+    age : 22,
+    single : true
+};
+
+// obj.age = "Hello";
+
+let arr = [1, 2, 3, 4];
+
+// arr.push("Hello");
+
+console.log(arr);
+console.log(obj);
